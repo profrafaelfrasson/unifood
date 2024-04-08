0b732e7c77d2b6132a2c2b6e65e654ae953463d8
profrafaelfrasson <164655959+profrafaelfrasson@users.noreply.github.com>
2024-04-07 21:20:12 -0300

    Initial commit

---
+++ src/test/java/br/com/unifood/unifood/UnifoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unifood.unifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
